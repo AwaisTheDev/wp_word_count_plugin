75950a8ea20b2f7f712453cd1b7774d61537366b
awais79 <itsme84006@gmail.com>
2022-04-29 06:45:53 +0500

    Added Post stats

--- word-count.php
+++ word-count.php
@@ -23,6 +23,38 @@ class wordCountPlugin
     {
         add_action('admin_menu', array($this, 'admin_settings_page'));
         add_action('admin_init', array($this, 'settings_page_options'));
+        add_filter('the_content', array($this, 'add_widget_to_content'));
+    }
+
+    public function add_widget_to_content($content)
+    {
+
+        if (is_main_query() and is_single()) {
+            return $this->createHTML($content);
+        }
+
+        return $content;
+    }
+
+    public function createHTML($content)
+    {
+
+        $html = "<h3>" . get_option('WCP_headline', "Post Stats ") . "</h3>";
+        $wordCount = str_word_count(wp_strip_all_tags($content));
+        $charCount = strlen(wp_strip_all_tags($content));
+        $readTime = round($wordCount / 200);
+
+        $html .= "<b>Word Count: </b>$wordCount <br>";
+        $html .= "<b>Character Count: </b>$charCount <br>";
+        $html .= "<b>Read time </b>: $readTime minute(s)";
+
+        if (get_option('WCP_location') == "0") {
+            return $html . $content;
+        } elseif (get_option('WCP_location') == "1") {
+            return $content . $html;
+
+        }
+
     }
     public function admin_settings_page()
     {
@@ -52,7 +84,7 @@ class wordCountPlugin
         add_settings_section('WCP_first_section', 'Subtitle', array($this, 'settings_section_callback'), 'word-count-settings');
 
         //Location
-        register_setting('WordCountPlugin', 'WCP_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => "1"));
+        register_setting('WordCountPlugin', 'WCP_location', array('sanitize_callback' => array($this, 'sanitize_location'), 'default' => "1"));
         add_settings_field('WCP_location_field', "Display Location", array($this, 'location_html_callback'), "word-count-settings", 'WCP_first_section');
         //Headline text
         register_setting('WordCountPlugin', 'WCP_headline', array('sanitize_callback' => 'sanitize_text_field', 'default' => "Heading"));
@@ -60,13 +92,22 @@ class wordCountPlugin
 
         //show word count
         register_setting('WordCountPlugin', 'WCP_wordCount', array('sanitize_callback' => 'sanitize_text_field', 'default' => "1"));
-        add_settings_field('WCP_wordCount', "Eidget headline", array($this, 'wordCount_html_callback'), "word-count-settings", 'WCP_first_section');
+        add_settings_field('WCP_wordCount', "Show headline", array($this, 'wordCount_html_callback'), "word-count-settings", 'WCP_first_section');
 
     }
 
     public function settings_section_callback()
     {
         echo "Out description here";
+    }
+
+    public function sanitize_location($input)
+    {
+        if ($input != "0" and $input != "1") {
+            add_settings_error('WCP_location', 'WCP_location_error', "Location must be either begining or end");
+            return get_option('WCP_location');
+        }
+        return $input;
     }
 
     public function location_html_callback()
